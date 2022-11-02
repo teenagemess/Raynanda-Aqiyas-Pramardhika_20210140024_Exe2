9cb5237de7ae7a6f675db261d7d0ebdd06fd150a
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-11-02 15:43:45 +0700

    membuat function program dan membuat method input

--- Excercise 2 Algoritma Semester 3/Program.cs
+++ Excercise 2 Algoritma Semester 3/Program.cs
@@ -8,8 +8,25 @@ namespace Excercise_2_Algoritma_Semester_3
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //Array to be searcher
+        int[] rap = new int[44];
+        //Number of element in the array
+        int n;
+        //get number of the element to store in the array
+        int i;
+
+       public void input()
         {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of the element in the array: ");
+                string s = (Console.ReadLine());
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 44))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements");
+            }
         }
     }
 }
